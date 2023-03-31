b29094c97f08f1bca879bb97026017c1e829dc16
BhaveshKumarReddy <128818065+BhaveshKumarReddy@users.noreply.github.com>
2023-03-31 16:18:32 +0530

    Delete EmployeeWithLocationDTO.cs

--- DTO_example/Models/EmployeeWithLocationDTO.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace DTO_example.Models
-{
-    public class EmployeeWithLocationDTO
-    {
-        public string Name { get; set; } = string.Empty;
-        public LocationDTO Location { get; set; } = new LocationDTO();
-    }
-}
